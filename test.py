8848ccbbe4f76aca4a416f9ea943705d6af89601
pavankumarreddy15 <ppavankr2001@gmail.com>
2020-07-20 22:36:45 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,6 @@
+from degrees import load_data
+from degrees import shortest_path
+from degrees import person_id_for_name
+from degrees import neighbors_for_person
+load_data(./Desktop/degrees/small)
+print(person_id_for_name("Kevin Bacon"))
